272ca95ea06de7eb2a9465899f862fd948f38578
Harvey Multani <harvey.multani@gmail.com>
2017-08-08 19:07:32 -0700

    Add cors

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
