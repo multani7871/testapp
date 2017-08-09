af8b3b79eadb9bc268aa5991be29fb3fd86ec5c3
Harvey Multani <harvey.multani@gmail.com>
2017-08-08 19:16:24 -0700

    Test sending text

--- server.js
+++ server.js
@@ -5,14 +5,9 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
- });
-
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  // res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  res.send('heyo');
 });
 
 var port = process.env.PORT || 8000;
